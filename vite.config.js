4d40b5991485b8a5b15eed8f2b45bb6e4c6bb1ea
Elaine <elaineakemi@gmail.com>
2024-05-02 16:12:10 +0100

    chore: update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import ssl from '@vitejs/plugin-basic-ssl'
 import { VitePWA } from 'vite-plugin-pwa'
-import path from 'path'
+import copy from 'rollup-plugin-copy'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
   plugins: [
-    react(),
+    react({ include: 'pathToAllReactFiles.{jsx,tsx}' }),
+    reactRefresh(),
     ssl(),
+    copy({
+      targets: [{ src: 'srcFilePath', dest: 'destinationPath' }],
+    }),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      devOptions: {
+        enabled: true,
+        type: 'module',
+      },
       manifest: {
         name: 'ARdverts',
         short_name: 'ARdverts',
@@ -51,10 +60,18 @@ export default defineConfig({
       // include: [/linked-dep/, /node_modules/],
     },
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+  esbuild: {
+    loader: 'jsx',
+    include: [
+      // Add this for business-as-usual behaviour for .jsx and .tsx files
+      'src/**/*.jsx',
+      'src/**/*.tsx',
+      'node_modules/**/*.jsx',
+      'node_modules/**/*.tsx',
+    ],
+    exclude: [],
   },
+
   server: { host: '0.0.0.0', https: true },
+  base: '/',
 })
